35f918dfe06e66c0a2b617c1000dc6973dc006af
Yusuf <easyclick06@gmail.com>
2025-09-10 23:55:16 +0100

    feat(rooutes): update routes to - include auth routes - lazy load some components - guard private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import DashboardLayout from "../layouts/dashboard";
 import DashboardHome from "../pages/dashboard/Dashboard";
+import RouteGuard from "./guard";
+import { lazy, Suspense } from "react";
+import MainLayout from "@/layouts/main";
 
-const AppRoutes = () => {
+const Signin = lazy(() => import("@/pages/auth/signin"));
+const SignUp = lazy(() => import("@/pages/auth/signup"));
+const Onboarding = lazy(() => import("@/pages/org/onboarding/onboard"));
+export default function AppRoutes() {
   return (
-    <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        {/* Auth Routes */}
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<DashboardHome />} />
+        {/* Public routes */}
+        <Route path="/signin/*" element={<Signin />} />
+        <Route path="/signup/*" element={<SignUp />} />
+
+        {/* Onboarding routes */}
+        <Route element={<RouteGuard />}>
+          <Route element={<MainLayout />}>
+            <Route path="/onboarding" element={<Onboarding />} />
+          </Route>
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RouteGuard />}>
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<DashboardHome />} />
+          </Route>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Suspense>
   );
-};
-
-export default AppRoutes;
+}
