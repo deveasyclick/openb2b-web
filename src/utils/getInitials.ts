ec31359bad0076faf32b7fe877a70f9d0da1853a
Yusuf <easyclick06@gmail.com>
2025-09-13 17:33:39 +0100

    feat(utils): add null checks for user names in `getInitials`

--- src/utils/getInitials.ts
+++ src/utils/getInitials.ts
@@ -1,11 +1,13 @@
 import type { User } from "@/types/user";
 
 const getInitials = (user: User) => {
+  if (!user.firstName && !user.lastName) return "";
+
   const firstName = user.firstName;
   const lastName = user.lastName;
   return (
-    firstName.charAt(0).toLocaleUpperCase() +
-    lastName.charAt(0).toLocaleUpperCase()
+    firstName?.charAt(0).toLocaleUpperCase() +
+    lastName?.charAt(0).toLocaleUpperCase()
   );
 };
 
